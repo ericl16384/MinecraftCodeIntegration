f8131b0d673e443bfd920ffc04ed4c83fb922e90
Eric <ericl16384@gmail.com>
2023-03-13 11:27:39 -0700

    improved test.sh hopefully

--- test.sh
+++ test.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+#if [ $# -eq 0 ]; then
+#  echo "Error: missing argument"
+#  exit 1
+#fi
+#MESSAGE=$1
+
+git status
+echo "Continue by adding all these files? (y/n)"
+while true; do
+    read -r yn
+    case $yn in
+        [Yy]* ) echo "You chose to continue."; break;;
+        [Nn]* ) echo "You chose to quit."; exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
+
+git add *
+
+echo "Type commit message to commit and push."
+IFS= read -r msg
+git commit -m "$msg"
+git push
+
+echo "Starting build and run."
+
 ./gradlew build && ./gradlew runClient
