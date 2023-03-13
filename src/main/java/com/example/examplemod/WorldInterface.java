2cbd5d5796f9fbb83f700dbe6ea308d990c8fd30
Eric <ericl16384@gmail.com>
2023-03-13 11:48:25 -0700

    switched yaw and pitch

--- src/main/java/com/example/examplemod/WorldInterface.java
+++ src/main/java/com/example/examplemod/WorldInterface.java
@@ -33,8 +33,8 @@ public class WorldInterface {
             y = player.getY();
             z = player.getZ();
 
-            yaw = player.getXRot();
-            pitch = player.getYRot();
+            yaw = player.getYRot();
+            pitch = player.getXRot();
 
             ExampleMod.LOGGER.debug("Current position: X=" + x + " Y=" + y + " Z=" + z);
             ExampleMod.LOGGER.debug("Current angles: YAW=" + yaw + " PITCH=" + pitch);
